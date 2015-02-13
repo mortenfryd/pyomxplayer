764f59c53d25a72f7a19f26f2ac28a442bbfa341
Morten Frydensberg <mortenfryd@gmail.com>
2015-02-13 17:49:19 +0100

    first commit

--- pyomxplayer.py
+++ pyomxplayer.py
@@ -11,14 +11,19 @@ class OMXPlayer(object):
     _AUDIOPROP_REXP = re.compile(r"Audio codec (\w+) channels (\d+) samplerate (\d+) bitspersample (\d+).*")
     _STATUS_REXP = re.compile(r"V :\s*([\d.]+).*")
     _DONE_REXP = re.compile(r"have a nice day.*")
+    _VOLUPDOWN_REXP = re.compile(r"Current Volume: (\d+.\d+)dB.*")
 
     _LAUNCH_CMD = '/usr/bin/omxplayer -s %s %s'
     _PAUSE_CMD = 'p'
     _TOGGLE_SUB_CMD = 's'
     _QUIT_CMD = 'q'
+    _VOLUP_CMD = '+'
+    _VOLDOWN_CMD = '-'
+    _WINSIZE_CMD = "--win \'%s %s %s %s\' "  
 
     paused = False
     subtitles_visible = True
+    volume = 0.0
 
     def __init__(self, mediafile, args=None, start_playback=False):
         if not args:
@@ -28,32 +33,28 @@ class OMXPlayer(object):
         
         self.video = dict()
         self.audio = dict()
-        # Get file properties
-        file_props = self._FILEPROP_REXP.match(self._process.readline()).groups()
-        (self.audio['streams'], self.video['streams'],
-         self.chapters, self.subtitles) = [int(x) for x in file_props]
+
         # Get video properties
-        video_props = self._VIDEOPROP_REXP.match(self._process.readline()).groups()
+        
+        video_props = self._VIDEOPROP_REXP.match(self._process.readline().decode('utf-8')).groups()
         self.video['decoder'] = video_props[0]
         self.video['dimensions'] = tuple(int(x) for x in video_props[1:3])
         self.video['profile'] = int(video_props[3])
         self.video['fps'] = float(video_props[4])
+        
         # Get audio properties
-        audio_props = self._AUDIOPROP_REXP.match(self._process.readline()).groups()
+        
+        audio_props = self._AUDIOPROP_REXP.match(self._process.readline().decode('utf-8')).groups()
         self.audio['decoder'] = audio_props[0]
         (self.audio['channels'], self.audio['rate'],
-         self.audio['bps']) = [int(x) for x in audio_props[1:]]
-
-        if self.audio['streams'] > 0:
-            self.current_audio_stream = 1
-            self.current_volume = 0.0
+        self.audio['bps']) = [int(x) for x in audio_props[1:]]
 
         self._position_thread = Thread(target=self._get_position)
         self._position_thread.start()
 
         if not start_playback:
             self.toggle_pause()
-        self.toggle_subtitles()
+
 
 
     def _get_position(self):
@@ -72,9 +73,31 @@ class OMXPlayer(object):
         if self._process.send(self._PAUSE_CMD):
             self.paused = not self.paused
 
-    def toggle_subtitles(self):
-        if self._process.send(self._TOGGLE_SUB_CMD):
-            self.subtitles_visible = not self.subtitles_visible
+    def vol_up(self):
+            self._process.send(self._VOLUP_CMD)
+            index = self._process.expect([self._VOLUPDOWN_REXP,
+                                            pexpect.TIMEOUT,
+                                            pexpect.EOF],timeout=1)
+            if index == 0:
+                self.volume = float(self._process.match.group(1))
+            
+
+    def vol_down(self):
+            self._process.send(self._VOLDOWN_CMD)
+            index = self._process.expect([self._VOLUPDOWN_REXP,
+                                            pexpect.TIMEOUT,
+                                            pexpect.EOF],timeout=1)
+            if index == 0:
+                self.volume = float(self._process.match.group(1))
+            
+
+    def set_display_size(self, x, y, width, height)
+        displaycmd = self._WINSIZE_CMD % (x, y, width, height)
+    
+    #def toggle_subtitles(self):
+    #    if self._process.send(self._TOGGLE_SUB_CMD):
+    #        self.subtitles_visible = not self.subtitles_visible
+            
     def stop(self):
         self._process.send(self._QUIT_CMD)
         self._process.terminate(force=True)
@@ -86,9 +109,6 @@ class OMXPlayer(object):
         raise NotImplementedError
 
     def set_subtitles(self, sub_idx):
-        raise NotImplementedError
-
-    def set_chapter(self, chapter_idx):
         raise NotImplementedError
 
     def set_volume(self, volume):
